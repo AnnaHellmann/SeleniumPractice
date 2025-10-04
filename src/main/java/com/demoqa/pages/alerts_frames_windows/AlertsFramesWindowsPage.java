b090c47121e16a33c689d8bbb3308a38b1877d60
Anna <hellmann@student.agh.edu.pl>
2025-10-04 17:15:53 +0200

    Windows Page

--- src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsFramesWindowsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsFramesWindowsPage.java
@@ -7,10 +7,22 @@ import static com.utilities.JavaScriptUtility.scrollToElementJS;
 
 public class AlertsFrameWindowsPage extends HomePage {
     private By modalDialogsCard = By.xpath("//span[text()='Modal Dialogs']");
+    private By alertsCard = By.xpath("//span[text()='Alerts']");
+    private By framesCard = By.xpath("//span[text()='Frames']");
 
     public ModalDialogsPage goToModalDialogs(){
         scrollToElementJS(modalDialogsCard);
         click(modalDialogsCard);
         return new ModalDialogsPage();
+    }
+    public AlertsPage goToAlertsPage(){
+        scrollToElementJS(alertsCard);
+        click(alertsCard);
+        return new AlertsPage();
+    }
+    public FramesPage goToFramesPage(){
+        scrollToElementJS(framesCard);
+        click(framesCard);
+        return new FramesPage();
     }
 }
---
+++ src/test/java/part3_4/com/demoqa/test/part3/alerts_frames_windows/WindowsTest.java
@@ -0,0 +1,4 @@
+package part3_4.com.demoqa.test.part3.alerts_frames_windows;
+
+public class WindowsTest {
+}
